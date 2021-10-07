047625b987e002f72da0a75c9f7f32d9144a2165
AlexxCourter <alex.courter@gmail.com>
2021-10-07 12:42:05 -0400

    Initial commit

---
+++ src/courter/Main.java
@@ -0,0 +1,27 @@
+package courter;
+
+/**
+ * Week 4 Prove assignment
+ * By Alex Courter
+ * */
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        //name of the first person to write the code:
+        String firstName = "Alex";
+        //reserved space for the names of others who edit:
+
+
+	//prints the name of the one who wrote this code
+        System.out.println("Hello, my name is " + firstName);
+
+    //others can put their edits here:
+
+
+        
+    }
+}
